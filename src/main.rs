03544c99018852613507da1e59852cc038762982
vedatcinbatexe <cinbatvedat@gmail.com>
2026-03-23 20:20:41 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello from Actix-web from on WSL!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Server starting at http://127.0.0.1:8080");
+    
+    HttpServer::new(|| {
+        App::new().service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
